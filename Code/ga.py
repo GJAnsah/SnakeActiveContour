7aba77808bce98ea86547475dc1e3a7a815c31a6
Simone <Simone>
2019-01-26 22:00:54 +0100

    image

--- Code/ga.py
+++ Code/ga.py
@@ -5,89 +5,100 @@ from skimage import data
 from scipy import ndimage
 from deap import creator, base, tools, algorithms
 
-%matplotlib inline
+#% matplotlib
+#inline
 
 import numpy as np
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import cv2
 
-
-alpha = 0.33 #controls continuity energy impact
-beta = 0.33 #controls curvatur energy impact
-gamma = 0.33 #controls area energy impact
+alpha = 0.2  # controls continuity energy impact
+beta = 0.2  # controls curvatur energy impact
+gamma = 0.6  # controls area energy impact
 
 
 def imageGradient(image):
-    sx = ndimage.sobel(image,axis=0,mode='constant')
-    sy = ndimage.sobel(image,axis=1,mode='constant')
-    #Get square root of sum of squares
-    sobel = np.hypot(sx,sy)
+    sx = ndimage.sobel(image, axis=0, mode='constant')
+    sy = ndimage.sobel(image, axis=1, mode='constant')
+    # Get square root of sum of squares
+    sobel = np.hypot(sx, sy)
     return (sobel)
 
-image = data.astronaut()
+image = cv2.imread('pic3.png')
+#image = data.astronaut()
 image = rgb2gray(image)
 imageGradients = imageGradient(image)
 
 
 def gradientEnergy(individual):
     eGrad = 0.0
-    for i in range(len(individual[0])-1):
+    for i in range(len(individual[0]) - 1):
         ind = individual[0][i]
         tmp_1 = imageGradients[ind[0]][ind[1]]
-        tmp_1 = -(abs(tmp_1)*abs(tmp_1))
+        tmp_1 = -(abs(tmp_1) * abs(tmp_1))
         eGrad = eGrad + tmp_1
-    return(eGrad)
+    return (eGrad)
+
 
 def areaEnergy(individual):
     A = 0.0
-    for i in range(len(individual[0])-1):
-        #x[i]* y[i+1] - x[i+1]*y[i]
-        tmp_1 = individual[0][i][0] * individual[0][i+1][1]
-        tmp_2 = individual[0][i+1][0] * individual[0][i][1]
+    for i in range(len(individual[0]) - 1):
+        # x[i]* y[i+1] - x[i+1]*y[i]
+        tmp_1 = individual[0][i][0] * individual[0][i + 1][1]
+        tmp_2 = individual[0][i + 1][0] * individual[0][i][1]
         A = A + (tmp_1 - tmp_2)
     A = abs(A)
-    return 0.5*A
+    return 0.5 * A
+
 
 def continuityEnergy(individual):
     cE = 0.0
-    for i in range(1,len(individual[0])):
-        #sqrt((x2-x1)^2 + (y2-y1)^2)
-        tmp_1 = individual[0][i][0] - individual[0][i-1][0]
-        tmp_2 = individual[0][i][1] - individual[0][i-1][1]
-        tmp_res = math.sqrt(tmp_1*tmp_1 + tmp_2*tmp_2)
+    for i in range(1, len(individual[0])):
+        # sqrt((x2-x1)^2 + (y2-y1)^2)
+        tmp_1 = individual[0][i][0] - individual[0][i - 1][0]
+        tmp_2 = individual[0][i][1] - individual[0][i - 1][1]
+        tmp_res = math.sqrt(tmp_1 * tmp_1 + tmp_2 * tmp_2)
         cE = cE + tmp_res
     return (cE)
 
+
 def smoothnessEnergy(individual):
     sE = 0.0
-    for i in range(1,len(individual[0])-1):
-        #|vi+1 - 2vi + vi-1|^2
-        tmp_1 = individual[0][i+1][0] - 2*individual[0][i][0] + individual[0][i-1][0]
-        tmp_2 = individual[0][i+1][1] - 2*individual[0][i][1] + individual[0][i-1][1]
-        tmp_res = math.sqrt(tmp_1*tmp_1 + tmp_2*tmp_2)
-        sE = sE+ tmp_res       
+    for i in range(1, len(individual[0]) - 1):
+        # |vi+1 - 2vi + vi-1|^2
+        tmp_1 = individual[0][i + 1][0] - 2 * individual[0][i][0] + individual[0][i - 1][0]
+        tmp_2 = individual[0][i + 1][1] - 2 * individual[0][i][1] + individual[0][i - 1][1]
+        tmp_res = math.sqrt(tmp_1 * tmp_1 + tmp_2 * tmp_2)
+        sE = sE + tmp_res
     return (sE)
 
+
 def internalFitness(individual):
-    return (alpha*continuityEnergy(individual)+beta*smoothnessEnergy(individual)+gamma*areaEnergy(individual))
+    return (alpha * continuityEnergy(individual) + beta * smoothnessEnergy(individual) + gamma * areaEnergy(individual))
+
 
 def externalFitness(individual):
     return (gradientEnergy(individual))
 
+
 def eval(individual):
-    return (internalFitness(individual)+externalFitness(individual),)
+    return (internalFitness(individual) + externalFitness(individual),)
+    #return (externalFitness(individual),)
 
 
-def mutSet(prop,individual):
+
+def mutSet(prop, individual):
     size = len(individual[0])
-    _x = random.randint(0, size-1)
+    _x = random.randint(0, size - 1)
     _y = random.randint(0, 1)
-    individual[0][_x][_y] += np.int(random.uniform(-10,10))
+    individual[0][_x][_y] += np.int(random.uniform(-10, 10))
     return individual,
 
+
 def cxTwoPointCopy(ind1, ind2):
-    #ind1 = ind1[0]
-    #ind2 = ind2[0]
+    # ind1 = ind1[0]
+    # ind2 = ind2[0]
     size = len(ind1[0])
     cxpoint1 = random.randint(0, size - 1)
     cxpoint2 = random.randint(0, size - 2)
@@ -101,31 +112,32 @@ def cxTwoPointCopy(ind1, ind2):
     ind2.fitness.values = eval(ind2)
     return ind1, ind2
 
+
 def initIndividual():
-    range_x = (100,150)
+    range_x = (100, 150)
     range_y = (200, 270)
-    range_r = (50,80)
+    range_r = (50, 80)
     _x = random.randint(min(range_x), max(range_x))
     _y = random.randint(min(range_y), max(range_y))
     _r = random.randint(min(range_r), max(range_r))
     s = np.linspace(0, 2 * np.pi, 600)
-    x = _x + _r * np.cos(s) #130 + 80 * np.cos(s)
-    y = _y + _r * np.sin(s) #250 + 57 * np.sin(s)
+    x = _x + _r * np.cos(s)  # 130 + 80 * np.cos(s)
+    y = _y + _r * np.sin(s)  # 250 + 57 * np.sin(s)
     V = np.array([x, y]).T
     V2 = V[::50]
-   
+
     x0, y0 = V2[:, 0].astype(np.int), V2[:, 1].astype(np.int)
 
     # store snake progress
     sn = np.array([x0, y0]).T
     return sn
 
-def genetic_algorithm( ):
+
+def genetic_algorithm():
     random.seed(86)
-    #init:
+    # init:
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", np.ndarray, fitness=creator.FitnessMin)
-
 
     toolbox = base.Toolbox()
     # register individuals
@@ -139,12 +151,11 @@ def genetic_algorithm( ):
     toolbox.register("select", tools.selTournament, tournsize=3)
     random.seed(64)
 
-    #run GA
-    pop = toolbox.population(n=300)
+    # run GA
+    pop = toolbox.population(n=150)
 
     for p in pop:
         p.fitness.values = eval(p)
-
 
     # Numpy equality function (operators.eq) between two arrays returns the
     # equality element wise, which raises an exception in the if similar()
@@ -158,7 +169,7 @@ def genetic_algorithm( ):
     stats.register("min", np.min)
     stats.register("max", np.max)
 
-    algorithms.eaSimple(pop, toolbox, cxpb=0.7, mutpb=0.0002, ngen=50, stats=stats, halloffame=hof)
+    algorithms.eaSimple(pop, toolbox, cxpb=0.7, mutpb=0.002, ngen=500, stats=stats, halloffame=hof)
 
     fig, ax = plt.subplots(figsize=(7, 7))
     ax.imshow(image, cmap=plt.cm.gray)
@@ -166,7 +177,6 @@ def genetic_algorithm( ):
 
     plt.show()
     return pop, stats, hof
-
 
 
 if __name__ == "__main__":
