7b86484db1c82c96d89b0b439597d4c5d86605b1
Michael Mayr <mayr.michael11@gmail.com>
2019-01-26 20:56:05 +0100

    Update ga.py
    Fitness implementation

--- Code/ga.py
+++ Code/ga.py
@@ -1,34 +1,90 @@
-import random
+import random, math
+import numpy as np
+from skimage import data
+from scipy import ndimage
 from deap import creator, base, tools, algorithms
+
+
+alpha = 0.33 #controls continuity energy impact
+beta = 0.33 #controls curvatur energy impact
+gamma = 0.33 #controls area energy impact
 
 import numpy as np
 
+def imageIntensity(image): 
+    return (0)
 
+def imageGradient(image):
+    sx = ndimage.sobel(image,axis=0,mode='constant')
+    sy = ndimage.sobel(image,axis=1,mode='constant')
+    #Get square root of sum of squares
+    sobel = np.hypot(sx,sy)
+    return (sobel)
 
-def eval(individual):
-    A = 0
-    individual = individual[0]
+imageGradients = imageGradient(data.astronaut())
+
+def gradientEnergy(individual):
+    eGrad = 0.0
+    for i in range(len(individual[0])-1):
+        ind = individual[0][i]
+        tmp_1 = imageGradients[ind[0]][ind[1]]
+        tmp_1 = -(abs(tmp_1)^2)
+        eGrad = eGrad + tmp_1
+    return(eGrad)
+
+def areaEnergy(individual):
+    A = 0.0
     for i in range(len(individual[0])-1):
         #x[i]* y[i+1] - x[i+1]*y[i]
-        tmp1 = individual[i][0] * individual[i+1][1]
-        tmp2 = individual[i+1][0] * individual[i][1]
-        A = A + (tmp1 - tmp2)
+        tmp_1 = individual[0][i][0] * individual[0][i+1][1]
+        tmp_2 = individual[0][i+1][0] * individual[0][i][1]
+        A = A + (tmp_1 - tmp_2)
     A = abs(A)
-    return 0.5*A,
+    return 0.5*A
 
-def mutSet(individual, prop):
-    size = len(individual)
-    _x = random.randint(1, size)
-    _y = random.randint(1, 2)
+def continuityEnergy(individual):
+    cE = 0.0
+    for i in range(1,len(individual[0])):
+        #sqrt((x2-x1)^2 + (y2-y1)^2)
+        tmp_1 = individual[0][i][0] - individual[0][i-1][0]
+        tmp_2 = individual[0][i][1] - individual[0][i-1][1]
+        tmp_res = math.sqrt(tmp_1*tmp_1 + tmp_2*tmp_2)
+        cE = cE + tmp_res
+    return (cE)
+
+def smoothnessEnergy(individual):
+    sE = 0.0
+    for i in range(1,len(individual[0])-1):
+        #|vi+1 - 2vi + vi-1|^2
+        tmp_1 = individual[0][i+1][0] - 2*individual[0][i][0] + individual[0][i-1][0]
+        tmp_2 = individual[0][i+1][1] - 2*individual[0][i][1] + individual[0][i-1][1]
+        tmp_res = math.sqrt(tmp_1*tmp_1 + tmp_2*tmp_2)
+        sE = sE+ tmp_res       
+    return (sE)
+
+def internalFitness(individual):
+    return (alpha*continuityEnergy(individual)+beta*smoothnessEnergy(individual)+gamma*areaEnergy(individual))
+
+def externalFitness(individual):
+    return (gradientEnergy(individual))
+
+def eval(individual):
+    return (internalFitness(individual)+externalFitness(individual),)
+
+
+def mutSet(prop,individual):
+    size = len(individual[0])
+    _x = random.randint(0, size-1)
+    _y = random.randint(0, 1)
     individual[0][_x][_y] += random.uniform(-10,10)
-    return individual
+    return individual,
 
 def cxTwoPointCopy(ind1, ind2):
     #ind1 = ind1[0]
     #ind2 = ind2[0]
     size = len(ind1[0])
-    cxpoint1 = random.randint(1, size)
-    cxpoint2 = random.randint(1, size - 1)
+    cxpoint1 = random.randint(0, size - 1)
+    cxpoint2 = random.randint(0, size - 2)
     if cxpoint2 >= cxpoint1:
         cxpoint2 += 1
     else:  # Swap the two cx points
@@ -71,7 +127,7 @@ def genetic_algorithm( ):
     # register crossover
     toolbox.register("mate", cxTwoPointCopy)
     toolbox.register("evaluate", eval)
-    toolbox.register("mutate", mutSet, indpb=0.05)
+    toolbox.register("mutate", mutSet, 0.05)
     toolbox.register("select", tools.selTournament, tournsize=3)
     random.seed(64)
 
